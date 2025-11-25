7ba937bf1e299c13f10d5a8421995936671118be
Oleksii Marushchak <OleksiiMarushchak@outlook.com>
2025-11-25 14:18:06 +0000

    update main.js

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import {
 const form = document.getElementById('search-form');
 const searchInput = form.querySelector('input[name="search-text"]');
 
-form.addEventListener('submit', async (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const query = searchInput.value.trim();
@@ -28,35 +28,39 @@ form.addEventListener('submit', async (e) => {
   clearGallery();
   showLoader();
 
-  try {
-    const data = await getImagesByQuery(query);
+  getImagesByQuery(query)
+    .then((data) => {
+      hideLoader();
 
-    hideLoader();
+      if (data.hits.length === 0) {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
 
-    if (data.hits.length === 0) {
-      iziToast.error({
-        title: 'Error',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
+      createGallery(data.hits);
+
+      iziToast.success({
+        title: 'Success',
+        message: `Found ${data.hits.length} images`,
         position: 'topRight',
       });
-      return;
-    }
 
-    createGallery(data.hits);
+      searchInput.value = '';
+    })
+    .catch((error) => {
+      hideLoader();
 
+      iziToast.error({
+        title: 'Error',
+        message: 'Failed to fetch images. Please try again.',
+        position: 'topRight',
+      });
 
-
-    searchInput.value = '';
-  } catch (error) {
-    hideLoader();
-
-    iziToast.error({
-      title: 'Error',
-      message: 'Failed to fetch images. Please try again.',
-      position: 'topRight',
+      console.error('Error fetching images:', error);
     });
-
-    console.error('Error fetching images:', error);
-  }
 });
